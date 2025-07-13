2d4e8a2eadeac0dad48ac7c59c7d16c3283bd3ed
khuhsiiakter <khushiiakterhena@gmail.com>
2025-07-13 18:16:27 +0600

    added post details page

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const getPostsDetails = async ({id})=>{
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const data = await res.json();
-    return data;
-}
-const PostDetails = async ({params}) => {
-    const p = await params;
-    console.log(p.id);
-    const postDetail = await getPostsDetails(p.id);
-    console.log(postDetail);
+const getPostsDetails = async ({ id }) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+};
+const PostDetails = async ({ params }) => {
+  // const p = await params;
+  const { id } = params;
+  // console.log(p.id);
+  // const postDetail = await getPostsDetails({p.id});
+  const postDetail = await getPostsDetails({ id });
+  console.log(postDetail);
   return (
-   <>
-
-    <h3>{postDetail.id}</h3>
-    <p>{postDetail.body}</p>
-   </>
-    
-  )
-}
+    <>
+      <div className=" border p-6 max-w-2/3 mx-auto">
+        <h3 className="text-3xl">{postDetail.id}</h3>
+        <p>{postDetail.body}</p>
+      </div>
+    </>
+  );
+};
 
 export default PostDetails;
