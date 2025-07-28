ef677c9377bafa518688464c4932d7574e9c4e6b
khuhsiiakter <khushiiakterhena@gmail.com>
2025-07-28 16:49:28 +0600

    added new branch

--- src/app/components/Login-btn.jsx
+++ src/app/components/Login-btn.jsx
@@ -16,7 +16,7 @@ const LoginBtn = () => {
   
   return (
     <div className="mt-6">
-       <h1 className="text-3xl font-bold underline">Welcome to Next.js!</h1>
+       <h1 className="text-3xl font-bold underline">Welcome to Next.js!!!!!</h1>
         <button onClick={() => signIn('google')} className='btn text-xl  btn-outline'>Sign In With Google</button>
     </div>
   )
